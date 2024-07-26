a97ac447e34a199c035a62bd9e4e5f3009dda109
NikitaNimse <nimsenikita710>
2024-07-26 14:10:50 +0530

    singup complate

--- client/src/views/SignUp/SignUp.js
+++ client/src/views/SignUp/SignUp.js
@@ -1,56 +1,59 @@
-import "./SignUp.css"
+import React, { useState } from 'react'
+import './SignUp.css'
 import axios from 'axios'
 import toast, {Toaster} from 'react-hot-toast'
+import { Link } from 'react-router-dom'
 
-import React, { useState } from 'react'
+function Signup() {
 
-function SignUp() {
-    const [user, setUser] = useState({
+  const [user, setUser] = useState({
+    fullname: '',
+    email: '',
+    password: '',
+    dob: ''
+  })
+
+  const signup = async () => {
+    const response = await axios.post(`${process.env.REACT_APP_bACKEND_URL}/signup`, {
+      fullname: user.fullname,
+      email: user.email,
+      password: user.password,
+      dob: user.dob
+    })
+
+    if(response.data.success){
+      toast.success(response.data.message)
+
+      setUser({
         fullname: '',
         email: '',
         password: '',
         dob: ''
       })
-      const signup = async () => {
-        const response = await axios.post(`${process.env.REACT_APP_bACKEND_URL}/signup`, {
-          fullname: user.fullname,
-          email: user.email,
-          password: user.password,
-          dob: user.dob
-        })
-    
-        if(response.data.success){
-          toast.success(response.data.message)
-    
-          setUser({
-            fullName: '',
-            email: '',
-            password: '',
-            dob: ''
-          })
-        }
-        else{
-          toast.error(response.data.message)
-        }
-      }
-  
+    }
+    else{
+      toast.error(response.data.message)
+    }
+  }
+
 
   return (
-    <div>
-        <h1 className="heading">User Registration</h1>
-        <form className='signup-form'>
+    <div className='signup-form'>
+      <h2>User Registration</h2>
+
+      <form >
         <input
           type="text"
           placeholder="Fullname"
-           className="input"
-          value={user.fullName}
-          onChange={(e) => setUser({ ...user, fullName: e.target.value})}
+          className='input'
+          value={user.fullname}
+          onChange={(e) => setUser({ ...user, fullname: e.target.value})}
           />
 
         <input
           type="email"
           placeholder="Email"
-           className="input"
+         className='input'
           value={user.email}
           onChange={(e)=>setUser({...user, email: e.target.value})}
           />
@@ -58,7 +61,7 @@ function SignUp() {
         <input
           type="password"
           placeholder="Password"
-           className="input"
+          className='input'
           value={user.password}
           onChange={(e)=>setUser({...user, password: e.target.value})}
           />
@@ -66,21 +69,25 @@ function SignUp() {
         <input
           type="date"
           placeholder="Date of Birth"
-          className="input"
+          className='input'
           value={user.dob}
           onChange={(e)=>setUser({...user, dob: e.target.value})}
           />
 
-        <button type="button" onClick={signup} className="form-btn">Register Now </button>
+        <button
+          type='button'
+          onClick={signup}
+          className='form-btn'
+          >
+          Register
+        </button>
+      </form>
 
-        </form>
-                 
+      <Link to='/login'>Already have an account? Login</Link>
 
-      
-        <Toaster />
-        
-        </div>
+      <Toaster />
+    </div>
   )
 }
 
-export default SignUp
+export default Signup
